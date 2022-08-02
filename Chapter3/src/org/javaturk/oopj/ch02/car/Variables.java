7d475c3ad268fc5ea1c001e3ee3482b1007ea3bb
elifnurdnmz <69845849+elifnurdnmz@users.noreply.github.com>
2022-08-02 18:24:09 +0300

    Add files via upload

---
+++ Chapter3/src/org/javaturk/oopj/ch02/car/Variables.java
@@ -0,0 +1,21 @@
+package org.javaturk.oopj.ch02.car;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		infoVariables();
+		
+	}	
+	
+	public static void infoVariables() {
+		String a = "A";
+		int b = 25;
+		double c = 3.45;
+		boolean d = true;
+		
+		System.out.println(a + " " + b + " " + c + " " + d );
+		
+		
+	}
+	
+}
